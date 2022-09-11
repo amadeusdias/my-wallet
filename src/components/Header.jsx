9443b786f8674816d69f2ddb725b2d79d8404ce2
AmadeusDias <amadeusdias@hotmail.com>
2022-09-11 19:28:13 -0300

    Cabeçalho da carteira, requisito dois

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { userEmail } = this.props;
+
     return (
-      <div>Header</div>
+      <div>
+        <h3 data-testid="email-field">{userEmail}</h3>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
     );
   }
 }
 
-const mapStateToProps = () => {};
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
 
 export default connect(mapStateToProps)(Header);
