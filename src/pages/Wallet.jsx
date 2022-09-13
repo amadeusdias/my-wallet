23be8f40c0cc5eb4b3ce69a1354dfb086d40d0d4
AmadeusDias <amadeusdias@hotmail.com>
2022-09-13 01:00:30 -0300

    tabela vazia, requisito 6

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { fetchCoins } from '../redux/actions/coinsAction';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   constructor() {
@@ -21,6 +22,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
